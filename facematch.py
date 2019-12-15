5ed8d4645d3552010d08150b0c0125f07e631dd6
sclosas <sclosas@gmail.com>
2019-12-14 22:26:55 -0300

    Cambios

--- facematch.py
+++ facematch.py
@@ -4,14 +4,19 @@ image_of_bill = face_recognition.load_image_file('./img/known/Bill Gates.jpg')
 bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
 
 unknown_image = face_recognition.load_image_file(
-    './img/unknown/d-trump.jpg')
+    './img/unknown/BG7.jpg')
 unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
 
 # Compare faces
 results = face_recognition.compare_faces(
     [bill_face_encoding], unknown_face_encoding)
 
+
+print(results)
+
 if results[0]:
     print('This is Bill Gates')
 else:
     print('This is NOT Bill Gates')
+
+
--- findfaces.py
+++ findfaces.py
@@ -1,9 +1,10 @@
 import face_recognition
 
-image = face_recognition.load_image_file('./img/groups/team2.jpg')
+image = face_recognition.load_image_file('./img/groups/team5.jpg')
 face_locations = face_recognition.face_locations(image)
 
 # Array of coords of each face
 # print(face_locations)
 
-print(f'There are {len(face_locations)} people in this image')
+
+print('En la foto hay ' + str(len(face_locations)) + ' personas')
